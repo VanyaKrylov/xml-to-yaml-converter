8151e6453f4e9e8e04e9e01f8bdfc0a67b13db79
VanyaKrylov <moonlightrider@mail.ru>
2018-12-28 11:05:44 +0300

    one shot commit guy is back at his business

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request
+import xmltodict
+import yaml
+
+"""@package docstring
+Documentation for the xml-to-yaml service
+The service uses RESTful API and receives POST requests with xml
+It sends the same data but in YAML format
+"""
+
+"""
+Creating the main app
+"""
+app = Flask(__name__)
+
+"""
+Setting a POST request handler
+Setting the service name to /xml/to/yaml/v1.0
+"""
+@app.route('/xml/to/yaml/v1.0', methods=['POST'])
+def convert():
+    """
+    Convert xml data to dictionary format and then dump to YAML format
+    :return:
+    YAML message
+    """
+    d = xmltodict.parse(request.data)
+    return yaml.dump(d, default_flow_style=False), 201
+
+
+"""
+Calling the handler
+"""
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
